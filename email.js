4aea94d8b64d2be502959708429266ba2f3c2e95
DefiledSpec <defiledspec@gmail.com>
2018-09-06 17:44:16 -0500

    added nodemailer functionallity for contact form

---
+++ email.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+
+module.exports = nodemailer.createTransport({
+	service: 'gmail',
+	secure: false,
+	port: 25,
+	auth: {
+		user: process.env.MAILUSERNAME,
+		pass: process.env.MAILPASSWORD
+	},
+	tls: {
+		rejectUnauthorized: false
+	}
+})
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const projects = require('../data/projects')
 const bio = require('../data/bio')
 const router = express.Router()
+const emailer = require('../email')
 
 router.get('/', (req, res) => {
 	res.render('about', { items: projects, bio })
@@ -17,6 +18,22 @@ router.get('/contact', (req, res) => {
 
 router.get('/resume', (req, res) => {
 	res.render('resume')
+})
+
+router.post('/contact', (req, res) => {
+	let { name, email, comment } = req.body
+	let emailOpts = {
+		from: `"${name.trim()}" <${email.trim()}`,
+		to: 'harrahjz@gmail.com',
+		subject: 'Portfolio Contact',
+		text: comment.trim()
+	}
+	emailer.sendMail(emailOpts, (err, info) => {
+		if(err) {
+			console.log(err)
+		}
+		res.render('about', { msg: 'Email Sent Successfully!', items: projects, bio })
+	})
 })
 
 module.exports = router
