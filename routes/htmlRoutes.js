e609ecd12d7d8f3937b1075a39747cbc9f40e68b
DefiledSpec <defiledspec@gmail.com>
2018-10-23 17:07:03 -0500

    updated web design

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,21 +22,25 @@ router.get('/resume', (req, res) => {
 
 router.post('/contact', (req, res) => {
 	let { name, email, comment } = req.body
-	let emailOpts = {
-		from: `"${name.trim()}" <${email.trim()}`,
-		to: 'harrahjz@gmail.com',
-		subject: 'Portfolio Contact',
-		text: comment.trim()
-	}
-	emailer.sendMail(emailOpts, (err, info) => {
-		if(err) {
-			console.log(err)
-			res.render('contact', { msg: 'There was a problem sending the email. Please try again!' })
-		} else {
-			res.render('contact', { msg: 'Email Sent Successfully!' })
+	if(name.trim() !== '' && email.trim() !== '' && comment.trim() !== '') {
+		let emailOpts = {
+			from: `"${name.trim()}" <${email.trim()}`,
+			to: 'harrahjz@gmail.com',
+			subject: 'Portfolio Contact',
+			text: comment.trim()
 		}
-		console.log(info)
-	})
+		emailer.sendMail(emailOpts, (err, info) => {
+			if(err) {
+				console.log(err)
+				res.render('contact', { msg: 'There was a problem sending the email. Please try again!' })
+			} else {
+				res.render('contact', { msg: 'Email Sent Successfully!' })
+			}
+			console.log(info)
+		})
+	} else {
+		res.render('contact', { msg: 'Name, Email and Message fields are required!' })
+	}
 })
 
 module.exports = router
